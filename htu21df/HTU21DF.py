42e2a706e9e6c64d297a82dddf136d76af99892f
Kostas Petrianakis <kpetrianakisgmail.com>
2020-07-07 13:36:55 +0300

    Moved HTU21DF files up

--- htu21df/HTU21DF.py
+++ htu21df/HTU21DF.py
@@ -44,36 +44,36 @@ rdreg = 0xE7
 reset = 0xFE
 
 def htu_reset():
-	handle = pi.i2c_open(bus, addr) # open i2c bus
-	pi.i2c_write_byte(handle, reset) # send reset command
-	pi.i2c_close(handle) # close i2c bus
-	time.sleep(0.2) # reset takes 15ms so let's give it some time
+    handle = pi.i2c_open(bus, addr) # open i2c bus
+    pi.i2c_write_byte(handle, reset) # send reset command
+    pi.i2c_close(handle) # close i2c bus
+    time.sleep(0.2) # reset takes 15ms so let's give it some time
 
 def read_temperature():
-	handle = pi.i2c_open(bus, addr) # open i2c bus
-	pi.i2c_write_byte(handle, rdtemp) # send read temp command
-	time.sleep(0.055) # readings take up to 50ms, lets give it some time
-	(count, byteArray) = pi.i2c_read_device(handle, 3) # vacuum up those bytes
-	pi.i2c_close(handle) # close the i2c bus
-	t1 = byteArray[0] # most significant byte msb
-	t2 = byteArray[1] # least significant byte lsb
-	temp_reading = (t1 * 256) + t2 # combine both bytes into one big integer
-	temp_reading = math.fabs(temp_reading) # I'm an idiot and can't figure out any other way to make it a float 
-	temperature = ((temp_reading / 65536) * 175.72 ) - 46.85 # formula from datasheet
-	return temperature
+    handle = pi.i2c_open(bus, addr) # open i2c bus
+    pi.i2c_write_byte(handle, rdtemp) # send read temp command
+    time.sleep(0.055) # readings take up to 50ms, lets give it some time
+    (count, byteArray) = pi.i2c_read_device(handle, 3) # vacuum up those bytes
+    pi.i2c_close(handle) # close the i2c bus
+    t1 = byteArray[0] # most significant byte msb
+    t2 = byteArray[1] # least significant byte lsb
+    temp_reading = (t1 * 256) + t2 # combine both bytes into one big integer
+    temp_reading = math.fabs(temp_reading) # I'm an idiot and can't figure out any other way to make it a float 
+    temperature = ((temp_reading / 65536) * 175.72 ) - 46.85 # formula from datasheet
+    return temperature
 
 def read_humidity():
-	handle = pi.i2c_open(bus, addr) # open i2c bus
-	pi.i2c_write_byte(handle, rdhumi) # send read humi command
-	time.sleep(0.055) # readings take up to 50ms, lets give it some time
-	(count, byteArray) = pi.i2c_read_device(handle, 3) # vacuum up those bytes
-	pi.i2c_close(handle) # close the i2c bus
-	h1 = byteArray[0] # most significant byte msb
-	h2 = byteArray[1] # least significant byte lsb
-	humi_reading = (h1 * 256) + h2 # combine both bytes into one big integer
-	humi_reading = math.fabs(humi_reading) # I'm an idiot and can't figure out any other way to make it a float
-	uncomp_humidity = ((humi_reading / 65536) * 125 ) - 6 # formula from datasheet
-	# to get the compensated humidity we need to read the temperature
-	temperature = read_temperature()
-	humidity = ((25 - temperature) * -0.15) + uncomp_humidity
-	return humidity
+    handle = pi.i2c_open(bus, addr) # open i2c bus
+    pi.i2c_write_byte(handle, rdhumi) # send read humi command
+    time.sleep(0.055) # readings take up to 50ms, lets give it some time
+    (count, byteArray) = pi.i2c_read_device(handle, 3) # vacuum up those bytes
+    pi.i2c_close(handle) # close the i2c bus
+    h1 = byteArray[0] # most significant byte msb
+    h2 = byteArray[1] # least significant byte lsb
+    humi_reading = (h1 * 256) + h2 # combine both bytes into one big integer
+    humi_reading = math.fabs(humi_reading) # I'm an idiot and can't figure out any other way to make it a float
+    uncomp_humidity = ((humi_reading / 65536) * 125 ) - 6 # formula from datasheet
+    # to get the compensated humidity we need to read the temperature
+    temperature = read_temperature()
+    humidity = ((25 - temperature) * -0.15) + uncomp_humidity
+    return humidity
--- htu21df/htu21df_test.py
+++ htu21df/htu21df_test.py
@@ -3,12 +3,22 @@
 import time
 import HTU21DF
 
+success = False
 while True:
-	#print("sending reset...")
-	HTU21DF.htu_reset
-	temperature = HTU21DF.read_temperature()
-	print("The temperature is %3.2f C." % round(temperature, 2))
-	time.sleep(1)
-	humidity = HTU21DF.read_humidity()
-	print("The humidity is %3.2f %%rh." % round(humidity,2))
-	time.sleep(60)
+	try:
+		#print("sending reset...")
+		HTU21DF.htu_reset
+		temperature = HTU21DF.read_temperature()
+		print("The temperature is %3.2f C." % round(temperature, 2))
+		time.sleep(1)
+		humidity = HTU21DF.read_humidity()
+		print("The humidity is %3.2f %%rh." % round(humidity,2))
+	        success = True
+		time.sleep(60)
+	except Exception as e:
+            print "error: %s"%(e)
+	    # wait a second for the retry
+            time.sleep(1)
+            success = False
+	if not success:
+	    print "Failed once! Retrying..."
